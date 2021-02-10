84cdc163c7bf75a6b172575140e13fe7fc50488c
Ann Schulte <ann.schulte@flatironschool.com>
2021-02-10 16:12:57 -0700

    merge main into spinner-component

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import image from '@rollup/plugin-image'
 import typescript from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
 import analyze from 'rollup-plugin-analyzer'
@@ -23,6 +24,7 @@ export default {
   ],
   external: ['styled-components'],
   plugins: [
+    image(),
     peerDepsExternal(),
     resolve(),
     commonjs(),
