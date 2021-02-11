ffbfe59b13ca6433c10e5760299ffb764be09965
Tom Milewski <tmilewski@gmail.com>
2021-02-10 23:36:28 -0500

    Remove module rules

--- .storybook/main.js
+++ .storybook/main.js
@@ -14,14 +14,6 @@ module.exports = {
     '@storybook/preset-create-react-app'
   ],
   webpackFinal: async config => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      loader: require.resolve('babel-loader'),
-      options: {
-        presets: [['react-app', { flow: false, typescript: true }]]
-      }
-    })
-
     config.resolve.alias['~'] = path.resolve(__dirname, '../src/')
     config.resolve.extensions.push('.ts', '.tsx')
 
