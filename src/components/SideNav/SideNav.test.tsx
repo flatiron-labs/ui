62dfb2608801974ecb2de4d1afebbcd0cefcdc3d
Ann Schulte <ann.schulte@flatironschool.com>
2021-02-11 11:31:25 -0700

    test fix

--- src/components/SideNav/SideNav.test.tsx
+++ src/components/SideNav/SideNav.test.tsx
@@ -3,27 +3,26 @@ import { render, screen } from '@testing-library/react'
 import * as useWindowWidth from '@react-hook/window-size'
 import SideNav from './SideNav'
 import SideNavItem from './SideNavItem/SideNavItem'
-import { Props } from './SideNav.types'
 
 describe('SideNav', () => {
   const testId = 'test-component'
 
-  const renderComponent = ({ children, ...props }: Props) => {
+  const renderComponent = () => {
     render(
-      <SideNav data-testid={testId} {...props}>
-        {children}
+      <SideNav data-testid={testId} isMobile={false} mobileOpen={false}>
+        <SideNavItem>Home</SideNavItem>
       </SideNav>
     )
   }
 
   it('should render a side nav', () => {
-    renderComponent({ children: <SideNavItem>Home</SideNavItem> })
+    renderComponent()
     screen.getByTestId(testId)
   })
 
   it('should render children', () => {
     jest.spyOn(useWindowWidth, 'useWindowWidth').mockImplementation(() => 900)
-    renderComponent({ children: <SideNavItem>Home</SideNavItem> })
+    renderComponent()
     expect(screen.getByTestId(testId)).toHaveTextContent('Home')
   })
 })
