24ff624195eb1993a593e9be022ea7bcae11047a
Ann Schulte <ann.schulte@flatironschool.com>
2021-02-11 09:27:15 -0700

    code review changes 3

--- src/components/Icons/Hamburger.tsx
+++ src/components/Icons/Hamburger.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Props from './icon.types'
 
-const HamburgerIcon = ({ color }: Props): JSX.Element => {
-  return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect x="2" y="3" width="20" height="3" rx="1" fill={color ?? 'white'} />
-      <rect x="2" y="8" width="20" height="3" rx="1" fill={color ?? 'white'} />
-      <rect x="2" y="13" width="20" height="3" rx="1" fill={color ?? 'white'} />
-    </svg>
-  )
-}
+const HamburgerIcon = ({ color }: Props): JSX.Element => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="2" y="3" width="20" height="3" rx="1" fill={color ?? 'white'} />
+    <rect x="2" y="8" width="20" height="3" rx="1" fill={color ?? 'white'} />
+    <rect x="2" y="13" width="20" height="3" rx="1" fill={color ?? 'white'} />
+  </svg>
+)
 
 export default HamburgerIcon
