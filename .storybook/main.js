918bc5a252d8e7ed810c63dc7629e736bce91b1d
Tom Milewski <tmilewski@gmail.com>
2021-02-10 23:57:01 -0500

    Use root tsconfig

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,9 @@
 const path = require('path')
 
+function resolve(input) {
+  return path.resolve(__dirname, input)
+}
+
 module.exports = {
   stories: [
     '../src/docs/Introduction.stories.mdx',
@@ -7,6 +11,15 @@ module.exports = {
     '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'
   ],
   addons: [
+    {
+      name: '@storybook/preset-typescript',
+      options: {
+        tsLoaderOptions: {
+          configFile: resolve('../tsconfig.json')
+        },
+        include: [resolve('../src')]
+      }
+    },
     '@etchteam/storybook-addon-status',
     '@storybook/addon-a11y',
     '@storybook/addon-links',
@@ -14,7 +27,7 @@ module.exports = {
     '@storybook/preset-create-react-app'
   ],
   webpackFinal: async config => {
-    config.resolve.alias['~'] = path.resolve(__dirname, '../src/')
+    config.resolve.alias['~'] = resolve('../src/')
     config.resolve.extensions.push('.ts', '.tsx')
 
     return config
