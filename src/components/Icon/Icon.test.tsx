b6b8abb2d43186a521bddb8cf68ebc835a8cb8e7
Tom Milewski <tmilewski@gmail.com>
2021-03-03 03:30:49 -0500

    Add snapshot testing

--- src/components/Icon/Icon.test.tsx
+++ src/components/Icon/Icon.test.tsx
@@ -8,156 +8,156 @@ describe('Icon', () => {
   describe('Code', () => {
     it('displays Code Icon', () => {
       render(<Icon.Code color="red" />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
 
     it('displays Code Icon with no args', () => {
       render(<Icon.Code />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
   })
 
   describe('Dial', () => {
     it('displays Dial Icon', () => {
       render(<Icon.Dial color="red" />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
 
     it('displays Dial Icon with no args', () => {
       render(<Icon.Dial />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
   })
 
   describe('Exit', () => {
     it('displays Exit Icon', () => {
       render(<Icon.Exit color="red" />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
 
     it('displays Exit Icon with no args', () => {
       render(<Icon.Exit />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
   })
 
   describe('Github', () => {
     it('displays Github Icon', () => {
       render(<Icon.Github color="red" />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
 
     it('displays Github Icon with no args', () => {
       render(<Icon.Github />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
   })
 
   describe('Hamburger', () => {
     it('displays Hamburger Icon', () => {
       render(<Icon.Hamburger color="red" />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
 
     it('displays Hamburger Icon with no args', () => {
       render(<Icon.Hamburger />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
   })
 
   describe('Home', () => {
     it('displays Home Icon', () => {
       render(<Icon.Home color="red" />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
 
     it('displays Home Icon with no args', () => {
       render(<Icon.Home />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
   })
 
   describe('Location', () => {
     it('displays Location Icon', () => {
       render(<Icon.Location color="red" />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
 
     it('displays Location Icon with no args', () => {
       render(<Icon.Location />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
   })
 
   describe('Lock', () => {
     it('displays Lock Icon', () => {
       render(<Icon.Lock color="red" />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
 
     it('displays Lock Icon with no args', () => {
       render(<Icon.Lock />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
   })
 
   describe('Mail', () => {
     it('displays Mail Icon', () => {
       render(<Icon.Mail color="red" />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
 
     it('displays Mail Icon with no args', () => {
       render(<Icon.Mail />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
   })
 
   describe('Map', () => {
     it('displays Map Icon', () => {
       render(<Icon.Map color="red" />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
 
     it('displays Map Icon with no args', () => {
       render(<Icon.Map />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
   })
 
   describe('Settings', () => {
     it('displays Settings Icon', () => {
       render(<Icon.Settings color="red" />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
 
     it('displays Settings Icon with no args', () => {
       render(<Icon.Settings />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
   })
 
   describe('Time', () => {
     it('displays Time Icon', () => {
       render(<Icon.Time color="red" />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
 
     it('displays Time Icon with no args', () => {
       render(<Icon.Time />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
   })
 
   describe('User', () => {
     it('displays User Icon', () => {
       render(<Icon.User color="red" />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
 
     it('displays User Icon with no args', () => {
       render(<Icon.User />)
-      expect(screen.findAllByRole(role)).toBeInTheDocument()
+      expect(screen.getByRole(role)).toMatchSnapshot()
     })
   })
 })
