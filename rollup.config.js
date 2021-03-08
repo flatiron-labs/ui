4b23118269e7005c2ecefdf389f63d219039f057
Tom Milewski <tmilewski@gmail.com>
2021-03-08 13:10:18 -0500

    Add assets to bundle

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import typescript from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
 import analyze from 'rollup-plugin-analyzer'
+import copy from 'rollup-plugin-copy'
 
 import pkg from './package.json'
 
@@ -25,6 +26,9 @@ export default {
   ],
   external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
   plugins: [
+    copy({
+      targets: [{ src: 'assets/*', dest: 'build' }]
+    }),
     peerDepsExternal(),
     resolve(),
     commonjs(),
@@ -32,7 +36,6 @@ export default {
       useTsconfigDeclarationDir: true,
       clean: true,
       rollupCommonJSResolveHack: true
-      // exclude: ['**/*.test.ts', '**/*.stories.ts', '**/*.test.tsx', '**/*.stories.tsx']
     }),
     image(),
     terser(),
