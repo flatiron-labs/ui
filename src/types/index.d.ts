9b6ce4383fd0f86cf726dfef6b1a6289b21341d3
Tom Milewski <tmilewski@gmail.com>
2021-03-23 00:09:26 -0400

    refactor(types): use import('react').* methodology
    When `import React` was top-level it wouold cause other `declare modules` to fail.

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,10 +1,12 @@
-import React from 'react'
-
 declare module '*.png'
 declare module '*.gif'
-declare module '*.svg'
-
-declare global {
-  type DefaultBreakpoints = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
-  type GetComponentProps<T> = T extends React.ComponentType<infer P> | React.Component<infer P> ? P : never
+declare module '*.svg' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const content: any
+  export default content
 }
+
+type DefaultBreakpoints = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+type GetComponentProps<T> = T extends import('react').ComponentType<infer P> | import('react').Component<infer P>
+  ? P
+  : never
