c1a62e86d6f6814e9fb099de06da9fd0bdc282e2
Tom Milewski <tmilewski@gmail.com>
2021-04-12 00:52:40 -0400

    chore: prep for cypress

--- src/components/Avatar/Avatar.test.tsx
+++ src/components/Avatar/Avatar.test.tsx
@@ -2,17 +2,25 @@ import React from 'react'
 import { render, screen } from '~/test/utils'
 import { Avatar, AvatarFallback, AvatarImage } from '~/components/Avatar'
 
-describe('Avatar', () => {
-  const initials = 'UI'
-
-  it('should render appropriate elements', () => {
+describe('<Avatar />', () => {
+  it('should render the image', () => {
     render(
       <Avatar>
-        <AvatarImage src="https://picsum.photos/id/1005/400/400" />
-        <AvatarFallback>{initials}</AvatarFallback>
+        <AvatarImage src="https://picsum.photos/id/1005/400/400" data-testid="image" />
+        <AvatarFallback>UI</AvatarFallback>
       </Avatar>
     )
 
-    screen.getByText(initials)
+    screen.getByText('UI')
+  })
+
+  it('should render the initials', () => {
+    render(
+      <Avatar>
+        <AvatarFallback>UI</AvatarFallback>
+      </Avatar>
+    )
+
+    screen.getByText('UI')
   })
 })
