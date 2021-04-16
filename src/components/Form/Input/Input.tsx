feada9b7525eb160926469284d79a29e1f9106a1
Tom Milewski <tmilewski@gmail.com>
2021-04-15 22:45:43 -0400

    refactor(input): refactor props

--- src/components/Form/Input/Input.tsx
+++ src/components/Form/Input/Input.tsx
@@ -6,42 +6,7 @@ import type { FieldError } from 'react-hook-form'
 import type { Icon } from 'phosphor-react'
 
 import { styled } from '~/styles/stitches.config'
-
 import { Label } from '~/components/Form/Label'
-
-type ValidFormInputTypes =
-  | 'button'
-  | 'checkbox'
-  | 'color'
-  | 'date'
-  | 'datetime-local'
-  | 'email'
-  | 'file'
-  | 'hidden'
-  | 'image'
-  | 'month'
-  | 'number'
-  | 'password'
-  | 'radio'
-  | 'range'
-  | 'reset'
-  | 'search'
-  | 'submit'
-  | 'tel'
-  | 'text'
-  | 'time'
-  | 'url'
-  | 'week'
-
-interface Props extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLInputElement>, HTMLInputElement> {
-  readonly label: string
-  readonly name: string
-  readonly type?: ValidFormInputTypes
-  readonly register?: any // eslint-disable-line @typescript-eslint/no-explicit-any
-  readonly disabled?: boolean
-  readonly help?: string
-  readonly icon?: Icon
-}
 
 /* -------------------------------------------------------------------------------------------------
  * Container
@@ -49,36 +14,6 @@ interface Props extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLInputEl
 
 const Container = styled('div', {
   position: 'relative',
-
-  input: {
-    background: 'transparent',
-    borderStyle: 'solid',
-    borderWidth: '2px',
-    color: '$white500',
-    fontFamily: '$firaCode',
-    fontSize: '$30',
-    height: '64px',
-    transition: 'border-color 0.3s ease',
-    width: '100%',
-
-    '&::placeholder': {
-      color: '$grey750',
-      transition: 'color 0.3s ease'
-    },
-
-    '&:focus::placeholder': {
-      color: 'transparent'
-    },
-
-    '&:focus': {
-      borderColor: '$cyan500',
-      outline: 0
-    },
-
-    '&:disabled': {
-      cursor: 'not-allowed'
-    }
-  },
 
   svg: {
     color: '$grey750',
@@ -95,29 +30,54 @@ const Container = styled('div', {
           color: '$white500'
         }
       }
-    },
+    }
+  }
+})
+
+/* -------------------------------------------------------------------------------------------------
+ * StyledInput
+ * -----------------------------------------------------------------------------------------------*/
+
+const StyledInput = styled('input', {
+  background: 'transparent',
+  borderColor: '$grey1000',
+  borderStyle: 'solid',
+  borderWidth: '2px',
+  color: '$white500',
+  fontFamily: '$firaCode',
+  fontSize: '$30',
+  height: '64px',
+  padding: '0 3em 0 1em',
+  transition: 'border-color 0.3s ease',
+  width: '100%',
+
+  '&::placeholder': {
+    color: '$grey750',
+    transition: 'color 0.3s ease'
+  },
+
+  '&:focus::placeholder': {
+    color: 'transparent'
+  },
+
+  '&:focus': {
+    borderColor: '$cyan500',
+    outline: 0
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed'
+  },
+
+  variants: {
     error: {
       true: {
-        input: {
-          borderColor: '$pink500'
-        }
-      },
-      false: {
-        input: {
-          borderColor: '$grey1000'
-        }
+        borderColor: '$pink500'
       }
     },
-    icon: {
+    hasIcon: {
       true: {
-        input: {
-          padding: '0 1em 0 3em'
-        }
-      },
-      false: {
-        input: {
-          padding: '0 3em 0 1em'
-        }
+        padding: '0 1em 0 3em'
       }
     }
   }
@@ -127,42 +87,55 @@ const Container = styled('div', {
  * Input
  * -----------------------------------------------------------------------------------------------*/
 
-export const Input: FCWithoutChildren<Props> = ({ type = 'text', name, label, help, icon, ...rest }) => {
-  const id = useId()
-  const { register, errors } = useFormContext()
-  const [active, setActive] = useState(false)
-  const setActiveState = () => setActive(!active)
-
-  const scopedErrors: FieldError | false = errors[name] ?? false
-  const helpId = `form-${name}-help`
-
-  const conditionalProps = {}
-
-  if (help) {
-    conditionalProps['aria-describedby'] = helpId
-  }
-
-  if (scopedErrors) {
-    conditionalProps['aria-invalid'] = true
-  }
-
-  return (
-    <Container error={!!scopedErrors} icon={!!icon} active={active}>
-      <Label id={id} name={name} label={label} helpId={helpId} helpText={help} errors={scopedErrors} />
-
-      {icon && React.createElement(icon)}
-
-      <input
-        aria-labelledby={id}
-        type={type}
-        name={name}
-        placeholder={label}
-        ref={register}
-        {...conditionalProps}
-        {...rest}
-        onFocus={setActiveState}
-        onBlur={setActiveState}
-      />
-    </Container>
-  )
+type Props = React.ComponentPropsWithRef<typeof StyledInput> & {
+  readonly help?: string
+  readonly icon?: Icon
+  readonly label: string
+  readonly name: string
 }
+
+export const Input = React.forwardRef<HTMLDivElement, Props>(
+  ({ type = 'text', name, label, help, icon, ...rest }, ref) => {
+    const fieldId = useId()
+    const { register, errors } = useFormContext()
+    const [active, setActive] = useState(false)
+    const setActiveState = () => setActive(!active)
+
+    const scopedErrors: FieldError | false = errors[name] ?? false
+    const helpId = `form-${name}-help`
+
+    const conditionalProps = {}
+
+    if (help) {
+      conditionalProps['aria-describedby'] = helpId
+    }
+
+    if (scopedErrors) {
+      conditionalProps['aria-invalid'] = true
+    }
+
+    return (
+      <Container active={active} ref={ref}>
+        <Label id={fieldId} name={name} label={label} helpId={helpId} helpText={help} errors={scopedErrors} />
+
+        {icon && React.createElement(icon)}
+
+        <StyledInput
+          {...conditionalProps}
+          {...rest}
+          aria-labelledby={fieldId}
+          error={!!scopedErrors}
+          hasIcon={!!icon}
+          name={name}
+          onFocus={setActiveState}
+          onBlur={setActiveState}
+          placeholder={label}
+          ref={register}
+          type={type}
+        />
+      </Container>
+    )
+  }
+)
+
+Input.displayName = 'Input'
