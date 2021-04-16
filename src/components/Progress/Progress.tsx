55bda0708e2e1b98c90394e78dd5bd154e1024bf
Ann Schulte <ann.schulte@flatironschool.com>
2021-04-16 12:28:21 -0700

    feat(progress): update progress ui

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -1,58 +1,41 @@
-import React, { ReactElement } from 'react'
+import React from 'react'
 import styled from 'styled-components'
+import * as RadixProgress from '@radix-ui/react-progress'
+import { flatironTheme } from '~/styles'
 
 export interface ProgressProps {
-  activeColor?: string
+  color?: string
   complete?: number
   'data-testid'?: string
-  fillColor?: boolean
   label?: string
   total?: number
 }
 
-const Label = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  padding-bottom: 5px;
-`
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-`
-
-const ItemContainer = styled.div`
-  padding-right: 5px;
-`
-
-const StyledProgress = styled.div<ProgressProps>`
-  background-color: ${props => (props.fillColor ? props.activeColor : '#5C5C5C')};
-  border-radius: 50%;
-  height: 5px;
-  width: 5px;
-`
-
-const createProgress = (complete: number, total: number, activeColor: string) => {
-  let progressBar: Array<ReactElement> = []
-
-  for (let i = 0; i < total; i += 1) {
-    const fill = complete > i
-    progressBar.push(
-      <ItemContainer>
-        <StyledProgress fillColor={fill} activeColor={activeColor} />
-      </ItemContainer>
-    )
-  }
-  return progressBar
+interface StyledProgressProps {
+  percent?: number | string
 }
 
+const StyledProgress = styled(RadixProgress.Root)`
+  position: relative;
+  height: 10px;
+  overflow: hidden;
+  border-radius: 5px;
+  background-color: ${flatironTheme.colors.common.greyDark};
+`
+const StyledIndicator = styled(RadixProgress.Indicator)<ProgressProps & StyledProgressProps>`
+  box-sizing: border-box;
+  position: absolute;
+  background-color: ${props => (props.color ? props.color : '#5C5C5C')};
+  height: 100%;
+  width: ${props => (props.percent ? `${props.percent}%` : 0)};
+`
+
 export const Progress = ({ ...props }: ProgressProps): JSX.Element => {
-  const { activeColor, complete, total, label, 'data-testid': dataTestId } = props
+  const { color, complete, total, 'data-testid': dataTestId } = props
+  const percent = (complete / total) * 100
   return (
-    <div data-testid={dataTestId}>
-      {label && <Label>{label}</Label>}
-      <Container>{createProgress(complete, total, activeColor)}</Container>
-    </div>
+    <StyledProgress aria-label="progressbar" role="progressbar" value={50} data-testid={dataTestId}>
+      <StyledIndicator color={color} percent={percent} />
+    </StyledProgress>
   )
 }
