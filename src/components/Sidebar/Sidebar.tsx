75614ae0ad23815deafbd0b21781d15f9249237b
Tom Milewski <tmilewski@gmail.com>
2021-05-04 00:10:02 -0400

    fix(sidebar): rebase issue

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,15 +4,6 @@ import { styled } from '~/styles/stitches.config'
  * Sidebar
  * -----------------------------------------------------------------------------------------------*/
 
-<<<<<<< HEAD
-const StyledSidebar = styled.div<StyledLinkProps>`
-  background-color: ${props => props.theme.colors.common.black};
-  min-height: 99%;
-  flex-direction: column;
-  border-right: 1px ${props => props.theme.colors.common.greyDarkest} solid;
-  position: absolute;
-  z-index: 555;
-=======
 export const Sidebar = styled('aside', {
   backgroundColor: '$black500',
   borderRight: '1px solid $grey1000',
@@ -23,7 +14,6 @@ export const Sidebar = styled('aside', {
   position: 'absolute',
   transition: 'width 150ms, height 150ms, transform 150ms ease-in-out',
   zIndex: 555,
->>>>>>> 63ce827 (feat: complete overhaul of components and styles)
 
   variants: {
     expanded: {
